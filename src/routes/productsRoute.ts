f1a27bfa364d1fc248817ceb45e0292633cebf03
Siigmaf <faby.tbe@gmail.com>
2023-02-08 11:08:46 -0300

    adiciona validação para rota de cadastro de produtos

--- src/routes/productsRoute.ts
+++ src/routes/productsRoute.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controller';
+import validateProductAmount from '../middlewares/validateProductAmount';
+import validateProductName from '../middlewares/validateProductName';
 
 const router = Router();
 
 const productController = new ProductController();
 
-router.get('/products', (req, res) => productController.getAll(req, res));
+router.use(validateProductName, validateProductAmount);
+
 router.post('/products', (req, res) => productController.create(req, res));
+router.get('/products', (req, res) => productController.getAll(req, res));
 
 export default router;
